074ccc76ea640dfdb04d40356f6777f58841022a
Naim Khan <naimkhan01856@gmail.com>
2024-07-02 11:02:14 +0600

    ascending sort done

---
+++ asc_sort_doubly_linked_list.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (tail == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+
+void sort(Node * head){
+    Node * temp = head;
+    for(Node * i = temp; i != NULL; i = i->next){
+        for(Node * j = i->next ; j != NULL; j = j->next){
+            if(i->val > j->val){
+                swap(i->val, j->val);
+            }
+        }
+    }
+   
+}
+
+
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while (true)
+    {
+        int val;
+        cin >> val;
+        if (val == -1)
+            break;
+        insert_at_tail(head, tail, val);
+    }
+    sort(head);
+    print(head);
+    
+    return 0;
+}
